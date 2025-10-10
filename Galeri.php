4c3def0bce0c335b7e84b9014ec2af540835deab
RayaNurIntan <arin.2006@smkn4-tsm.sch.id>
2025-10-10 09:37:21 +0700

    first commit

--- Galeri.php
+++ Galeri.php
@@ -19,156 +19,60 @@
   <body>
     <?php include 'navbar.php';?>
 
-    <!-- SECTION GALERI -->
-         <?php
-            $hut = [
-              "galeri/1.jpg",
-              "galeri/2.jpg",
-              "galeri/3.jpg",
-              "galeri/4.jpg",
-              "galeri/5.jpg",
-              "galeri/6.jpg"
+    <?php
+      $hut = [
+        "galeri/1.jpg",
+        "galeri/2.jpg",
+        "galeri/3.jpg",
+        "galeri/4.jpg",
+        "galeri/5.jpg",
+        "galeri/6.jpg"
+      ];
 
-            ];
-           
-         ?>
-   
+      $galeri = [
+        "galeri/7.jpg",
+        "galeri/8.jpg",
+        "galeri/9.jpg",
+        "galeri/10.jpg",
+        "galeri/11.jpg",
+        "galeri/0.jpg"
+      ];
+    ?>
+
+    <!-- HUT RI -->
     <div class="card shadow-lg rounded-2 mt-3">
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="card border-0">
-            <div class="text-center mb-4 mt-3">
-              <h5>Upacara Peringatan HUT RI Ke-80</h5>
-              <hr class="w-25 mx-auto" />
-            </div>
-          </div>
-        </div>
+      <div class="text-center mb-4 mt-3">
+        <h5>Upacara Peringatan HUT RI Ke-80</h5>
+        <hr class="w-25 mx-auto" />
       </div>
       <div class="row m-2">
-          <?php foreach ($hut as $gambar): ?>
-        <div class="col-lg-4">
-          <div class="card border-0">
-            <img src="<?=$gambar?>" class="img-fluid" alt="" />
-          </div>
-        </div>
-        <?php endforeach;?>
-         <?php
-            $galeri = [
-              'galeri/7.jpg',
-              'galeri/8.jpg',
-              'galeri/9.jpg',
-              'galeri/10.jpg',
-              'galeri/11.jpg',
-              'galeri/7.jpg'
-
-            ];
-           
-         ?>
-   
-    <div class="card shadow-lg rounded-2 mt-3">
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="card border-0">
-            <div class="text-center mb-4 mt-3">
-              <h5>MPLS tahun ajaran 2025</h5>
-              <hr class="w-25 mx-auto" />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row m-2">
-          <?php foreach ($hut as $gambar): ?>
-        <div class="col-lg-4">
-          <div class="card border-0">
-            <img src="<?=$gambar?>" class="img-fluid" alt="" />
-          </div>
-        </div>
-        
-        <?php endforeach;?>
-
-    <!-- SECTION FOOTER -->
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="row">
-          <div class="col-lg-5">
+        <?php foreach ($hut as $gambar): ?>
+          <div class="col-lg-4 col-md-6 mb-3">
             <div class="card border-0">
-              <div class="card-body">
-                <h2>Alamat</h2>
-                <hr />
-                <div>
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.2131636317513!2d108.25189907357232!3d-7.329939972086937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f59eeb6bda36f%3A0xa9e724a275da6c2d!2sSMK%20Negeri%204%20Tasikmalaya!5e0!3m2!1sid!2sid!4v1755671496167!5m2!1sid!2sid"
-                    width="400"
-                    height="300"
-                    style="border: 0"
-                    allowfullscreen=""
-                    loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"
-                  ></iframe>
-                </div>
-              </div>
+              <img src="<?= $gambar ?>" class="img-fluid rounded" alt="Galeri HUT" />
             </div>
           </div>
-          <div class="col-lg-4">
-            <div class="card border-0">
-              <div class="card-body">
-                <h2>Kontak</h2>
-                <hr />
-                <p class="bi bi-telephone m-1">085770605539</p>
-                <p class="bi bi-envelope">SMKN4@gmail.com</p>
-                <h2>Media Sosial</h2>
-                <hr />
-                <div class="d-flex gap-3">
-                  <i class="bi bi-facebook"></i>
-                  <i class="bi bi-instagram"></i>
-                  <i class="bi bi-tiktok"></i>
-                  <i class="bi bi-youtube"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-3">
-            <div class="card border-0">
-              <div class="card-body">
-                <h2>Link lainnya</h2>
-                <hr />
-                <a
-                  class="text-decoration-none text-black"
-                  href="https://kemendikdasmen.go.id/"
-                  >Kemendikbud</a
-                >
-                <br />
-                <a
-                  class="text-decoration-none text-black"
-                  href="https://kemendikdasmen.go.id/"
-                  >Dapodiknasmen</a
-                >
-                <br />
-                <a
-                  class="text-decoration-none text-black"
-                  href="https://kemendikdasmen.go.id/"
-                  >PSMK</a
-                >
-                <br />
-                <a
-                  class="text-decoration-none text-black"
-                  href="https://kemendikdasmen.go.id/"
-                  >Disdik Jabar</a
-                >
-              </div>
-            </div>
-          </div>
-          <div class="text-center">
-            <hr />
-            <small
-              >© 2025. SMK Negeri 4 Tasikmalaya.<br />
-              All rights reserved.</small
-            >
-            <hr />
-          </div>
-        </div>
+        <?php endforeach; ?>
       </div>
     </div>
-    <!-- TUTUP SECTION FOOTER -->
+
+    <!-- MPLS -->
+    <div class="card shadow-lg rounded-2 mt-3">
+      <div class="text-center mb-4 mt-3">
+        <h5>MPLS Tahun Ajaran 2025</h5>
+        <hr class="w-25 mx-auto" />
+      </div>
+      <div class="row m-2">
+        <?php foreach ($galeri as $gambar): ?>
+          <div class="col-lg-4 col-md-6 mb-3">
+            <div class="card border-0">
+              <img src="<?= $gambar ?>" class="img-fluid rounded" alt="Galeri MPLS" />
+            </div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    </div>
+
+   <?php include 'footer.php';?>
   </body>
 </html>
--- visimisi.php
+++ visimisi.php
@@ -19,57 +19,121 @@
   <body>
     <?php include 'navbar.php';?>
 
-    <!-- SECTION VISI -->
-
-    <div class="container px-5 mt-5">
-      <h3 class="mb-0">Visi&</h3>
-      <h3 class="text-info">Misi</h3>
-      <div class="col-lg-12">
-        <div class="card shadow rouded bg-primary text-center text-white">
-          <h2 class="text-center">Visi</h2>
-          <p class="text-center">
-            Terwujudnya lulusan yang Cerdas, Aktif, Kompetitif, Adaptif dan
-            Produktif berlandaskan Iman dan Takwa <br />
-            (CAKAP BERIMTAK)
-          </p>
+    <!-- VISI -->
+        <div class="container my-5">
+          <div class="card bg-primary text-white text-center p-4">
+            <h2>Visi</h2>
+            <p class="mb-0">
+              Terwujudnya lulusan yang Cerdas, Aktif, Kompetitif, Adaptif dan
+              Produktif berlandaskan Iman dan Takwa (CAKAP BERIMTAK)
+            </p>
+          </div>
         </div>
-      </div>
-    </div>
+        <!-- END VISI -->
+        <!--VISI -->
+        <div class="container my-5">
+         <div class="card bg-primary text-white p-4">
+            <h2 class="text-center">Misi</h2>
+            <ol>
+              <li>
+                Menyiapkan Sumber Daya Manusia yang cerdas dan kompeten baik
+                hardskill maupun softskill.
+              </li>
+              <li>
+                Meningkatkan dan mengembangkan aktifitas serta kreatifitas
+                seluruh warga sekolah dalam berbagai kegiatan positif.
+              </li>
+              <li>
+                Mewujudkan manajemen pengelolaan yang efektif, efisien,
+                transparan, akuntabel dan layanan prima.
+              </li>
+              <li>
+                Menumbuhkan dan mengembangkan potensi Sumber Daya Manusia yang
+                berdaya saing tinggi melalui kegiatan akademik maupun non
+                akademik.
+              </li>
+              <li>
+                Menyelenggarakan berbagai program kegiatan dalam upaya
+                meningkatkan SDM yang mampu menyesuaikan dengan perkembangan.
+              </li>
+              <li>
+                Mengembangkan sarana prasarana dan lingkungan sekolah yang
+                menyenangkan sebagai wadah inovasi.
+              </li>
+              <li>
+                Membangun jiwa wirausaha yang handal melalui pembelajaran
+                Teaching Factory (TEFA) dan Kelas Industri.
+              </li>
+              <li>
+                Menjalin kemitraan dengan IDUKA serta lembaga lainnya yang
+                relevan.
+              </li>
+              <li>
+                Menanamkan dan membudayakan sikap serta perilaku yang baik pada
+                aktivitas di sekolah maupun kehidupan sehari-hari.
+              </li>
+            </ol>
+          </div>
+        </div>
+        <!-- END MISI -->
+        <!-- TUJUAN -->
+        <div class="container my-5">
+          <div class="card bg-primary text-white p-4">
+            <h2 class="text-center">Tujuan SMK Negeri 4 Tasikmalaya</h2>
+            <ol>
+              <li>
+               1.Sekolah meraih prestasi pencapaian nilai rata-rata UN dan indeks integritas diatas rata-rata Nasional pada tahun 2026.
+              </li>
+              <li>
+              2.Sekolah Menghasilkan Tamatan yang siap bekerja Minimal 50 % Tamatan bekerja pada tahun tamat.
+              </li>
+              <li>
+               3.Terciptanya aktivitas dan kreatifitas yang positif oleh seluruh warga sekolah.
+              </li>
+              <li>
+               4.Sekolah meraih prestasi akademik dan non akademik minimal di tingkat provinsi Jawa Barat dan Nasional.
+              </li>
+              <li>
+              5.Sekolah memiliki tenaga pendidik dan kependidikan yang berkualifikasi minimal S1 dan linier dengan tugasnya, bersertifikat pendidik
+               dan telah mengikuti diklat implementasi kurikulum merdeka pada tahun 2024.
+              </li>
+              <li>
+             6.Sekolah mencapai kriteria sekolah berwawasan lingkungan (adiwiyata) pada tahun 2026
+              </li>
+              <li>
+              7.Sekolah terakreditasi dengan Nilai A pada tahun 2024.
+              </li>
+              <li>
+              8.Sekolah memiliki Lembaga Sertifikasi Profesi (LSP) yang terlisensi, Tempat Uji Kompetensi (TUK) yang terverifikasi dan Bursa Kerja Khusus (BKK) yang terakreditasi pada tahun 2025.
+              </li>
+              <li>
+              9.Sekolah memiliki Perpustakaan yang terakreditasi A pada tahun 2026.
+              </li>
+              <li>
+              10.Sekolah menjadi Badan Layanan Umum Daerah (BLUD) pada tahun 2025.
+              </li>
+              <li>
+              11.Sekolah mengimplementasikan Kurikulum Merdeka untuk seluruh tingkat pada tahun 2024.
+              </li>
+              <li>
+              12.Sekolah memiliki Dokumen Kurikulum Merdeka untuk seluruh tingkat pada tahun 2024.
+              </li>
+              <li>
+              13.Sekolah menerapkan Kurikulum Merdeka dengan Pembelajaran Berdiferensiasi dan Pembelajaran Sosial-Emosional untuk seluruh tingkat pada tahun 2024.
+              </li>
+              <li>
+              14.Sekolah memiliki sarana prasaranasesuai dengan standar pelayanan minimal pendidikan dan standar IDUKA pada tahun 2026.
+              </li>
+              <li>
+              15.Sekolah memiliki Teaching Factory di semua program keahlian yang sesuai dengan standar industri dan representatif pada tahun 2024.
+              </li>
+              <li>
+              16.Sekolah melaksanakan program menumbuh kembangkan Budi Pekerti dan akhlak mulia melalui pendidikan karakter pada kegiatan, intra, ekstra kurikuler dan non kurikuler.
+              </li>
+          </div>
+        </div>
+        <!-- END TUJUAN -->
 
-    <!-- TUTUP VISI -->
-
-    <!-- SECTION MISI -->
-    <div class="card mt-5 P-5 mx-5">
-      <h2>misi</h2>
-      <p>1. Menyiapkan Sumber Daya Manusia yang cerdas dan kompeten baik
-        hardskill maupun softskill.</p>
-
-        <P>2. Meningkatkan dan mengembangkan aktifitas serta kreatifitas seluruh
-        warga sekolah dalam berbagai kegiatan positif.</P>
-
-        <P>3. Mewujudkan manajemen pengelolaan yang efektif, efesien, transparan,
-        akuntabel dan layanan prima.</P>
-
-        4. Menumbuhkan dan mengembangkan potensi dan kemampuan Sumber Daya
-        Manusia yang berdaya saing tinggi melalui berbagai kegiatan akademik
-        maupun non akademik.</P>
-
-       <p> 5. Menyelenggarakan berbagai program kegiatan dalam upaya meningkatkan
-        Sumber Daya Manusia yang mampu menyesuaikan dengan perkembangan.</P>
-
-       <P> 6. Mengembangkan sarana prasarana dan lingkungan sekolah yang
-        menyenangkan sebagai wadah menumbuhkembangkan daya kreasi dan inovasi
-        untuk menghasilkan produk teknologi tepat guna.</P>
-
-       <P> 7. Membangun jiwa wirausaha yang handal melalui pembelajaran Teaching
-        Factory (TEFA) dan Kelas Industri.</P>
-
-       <P> 8. Menjalin dan mengembangkan kemitraan dengan Industri dan Dunia Kerja
-        (IDUKA) serta lembaga lainnya yang relevan.</P>
-
-       <P> 9. Menanamkan dan membudayakan sikap dan perilaku yang baik pada
-        aktivitas di sekolah maupun dalam kehidupan sehari-hari.</P>
-
-      <?php include 'navbar.php';?>
+      <?php include 'footer.php';?>
   </body>
 </html>
